fe9e9c39d46720b1ea725b438c3fa431dda0823f
Eric Frey <ericlfrey@gmail.com>
2022-12-10 07:56:09 -0600

    Submit Order Works

--- functions/submitOrder.js
+++ functions/submitOrder.js
@@ -1,7 +1,8 @@
 import { patchOrder, postOrder } from '../api/orderData';
-// import { viewOrders } from '';
+import viewOrders from './viewOrders';
 
-const submitOrder = () => {
+const submitOrder = (e) => {
+  e.preventDefault();
   const payload = {
     name: document.querySelector('#createOrderName').value,
     phone: document.querySelector('#createOrderPhone').value,
@@ -13,7 +14,7 @@ const submitOrder = () => {
   console.warn(payload);
   postOrder(payload).then(({ name }) => {
     const patchPayload = { firebaseKey: name };
-    patchOrder(patchPayload).then();
+    patchOrder(patchPayload).then(viewOrders);
   });
 };
 
